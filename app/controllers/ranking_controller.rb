5f871f0a7b5f094f0c6546ae0a06514ce62fad03
Shinya Katagi <yanshigi.2.24@gmail.com>
2017-02-27 23:49:21 +0000

    modify ranking

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -2,12 +2,16 @@ class RankingController < ApplicationController
     
     def want
         want_rank_id=Want.group(:item_id).order("count_all desc").count.keys.first(10)
-            @items=Item.find(want_rank_id)
+        @items=Item.find(want_rank_id).sort_by! do |item|
+            want_rank_id.index(item.id)
+        end
     end
     
     def have
         have_rank_id=Have.group(:item_id).order("count_all desc").count.keys.first(10)
-            @items=Item.find(have_rank_id)
+        @items=Item.find(have_rank_id).sort_by! do |item|
+            have_rank_id.index(item.id)
+        end
     end
     
 end
